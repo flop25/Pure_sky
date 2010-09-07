eda9ed7cce94874effa10fa7cb19cd064cd74c50
flop25 <flop25@piwigo.org>
2010-09-07 13:25:23 +0000

    version 1.0.0 for pwg 2.1.x
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_sky@6867 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Theme Name: Pure_sky
+Version: 1.0.0
+Description: Dans le bleu du ciel abandonnez-vous ...
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=446
+Author: flop25
+Author URI: http://www.planete-flop.fr/site.php?acc=file
+*/
+
+$themeconf = array(
+  'name'         => 'Pure_sky',
+  'parent'        => 'Pure_default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+);
+
+?>
